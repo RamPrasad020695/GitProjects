fc31c45c2813651f120418f0d06a44bca8ff9fd7
RamPrasad020695 <prasadram0206@gmail.com>
2022-09-07 07:55:32 +0530

    Prasad's code

--- src/test/java/org/test/GitTesting.java
+++ src/test/java/org/test/GitTesting.java
@@ -2,8 +2,8 @@ package org.test;
 
 public class GitTesting {
 public static void main(String[] args) {
-	System.out.println("Ram");
-	System.out.println("divi");
-	System.out.println("vishnu");
+	System.out.println("Prasad");
+	System.out.println("Prabha");
+	System.out.println("Prasath");
 }
 }
